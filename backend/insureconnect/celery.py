8d839f4bfa8ab54b76631db37f0e4ca15d6bcda2
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-08-24 18:23:38 +0530

    Added requirments, Dockerfile

---
+++ backend/insureconnect/celery.py
@@ -0,0 +1,9 @@
+# insureconnect/celery.py
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insureconnect.settings")
+
+app = Celery("insureconnect")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
